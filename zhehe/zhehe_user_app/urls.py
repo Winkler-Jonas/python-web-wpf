78c5dd8709d11bf906f69540a86f41d5c3ad5603
jonaswi <jonas.winkler@hs-augsburg.de>
2020-07-19 05:10:28 +0200

    fixing url home

--- zhehe/zhehe_user_app/urls.py
+++ zhehe/zhehe_user_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    re_path(r'^home/', views.home, name='home'),
+    re_path(r'^home/', views.home),
     re_path(r'^contact/', views.contact, name='contact'),
     re_path(r'^download/(?P<note>.+)/', views.download, name='download'),
     re_path(r'new/', views.new_doc, name='new_doc')
